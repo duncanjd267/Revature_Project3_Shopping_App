82c4b606df047012620d8c4273623f799a45ecd9
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-24 10:21:59 -0400

    Added Login View Controller and DBHelper, pulled from main.

---
+++ Project_3/Project_3/DBHelper.swift
@@ -0,0 +1,19 @@
+//
+//  DBHelper.swift
+//  Project_3
+//
+//  Created by admin on 5/24/21.
+//
+
+import Foundation
+import CoreData
+
+
+class DBHelper
+{
+	static var inst = DBHelper()
+	
+	let context = SceneDelegate().context
+	
+	
+}
---
+++ Project_3/Project_3/LoginCtrl.swift
@@ -0,0 +1,20 @@
+//
+//  LoginCtrl.swift
+//  Project_3
+//
+//  Created by admin on 5/24/21.
+//
+
+import UIKit
+
+class LoginCtrl: UIViewController
+{
+
+    override func viewDidLoad()
+	{
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
