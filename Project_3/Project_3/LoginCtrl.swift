f3aa22c8e1cfc47efb05df6575006ed22688c5c2
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-24 15:29:47 -0400

    Did big work on login button action

--- Project_3/Project_3/LoginCtrl.swift
+++ Project_3/Project_3/LoginCtrl.swift
@@ -16,9 +16,31 @@ class LoginCtrl: UIViewController
 	@IBAction func LogButton(_ sender: UIButton)
 	{
 		var usrN = UserField.text
-		var passN = PassField.text
+		var passW = PassField.text
 		
+		if let emp = UserField.text, emp.isEmpty
+		{ // no point in continuing without a username
+			return
+		}
 		
+		let Usr = DBHelper.inst.getOneUser(user: usrN!)
+		
+		if let emp = PassField.text, emp.isEmpty
+		{ // no point in continuing without the password either
+			return
+		}
+		
+		let passAct = Usr.password
+		
+		if passW == passAct
+		{
+			// Verification succeeds
+		}
+		else
+		{
+			// Failure
+			return
+		}
 	}
 	
 	override func viewDidLoad()
