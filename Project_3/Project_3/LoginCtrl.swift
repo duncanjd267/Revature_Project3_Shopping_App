bd4a716627834e42738ea7ec260de400c0644981
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-24 14:59:12 -0400

    began work on Login action

--- Project_3/Project_3/LoginCtrl.swift
+++ Project_3/Project_3/LoginCtrl.swift
@@ -10,7 +10,18 @@ import UIKit
 class LoginCtrl: UIViewController
 {
 
-    override func viewDidLoad()
+	@IBOutlet weak var UserField: UITextField!
+	@IBOutlet weak var PassField: UITextField!
+	
+	@IBAction func LogButton(_ sender: UIButton)
+	{
+		var usrN = UserField.text
+		var passN = PassField.text
+		
+		
+	}
+	
+	override func viewDidLoad()
 	{
         super.viewDidLoad()
 
