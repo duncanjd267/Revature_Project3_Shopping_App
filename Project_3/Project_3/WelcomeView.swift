68088d97a79d25f29b104edd6e71ade296909bed
duncanjd267 <duncanjd267@gmail.com>
2021-05-26 12:28:16 -0400

    DBHelper is working properly, database is properly retrieving data

--- Project_3/Project_3/WelcomeView.swift
+++ Project_3/Project_3/WelcomeView.swift
@@ -21,13 +21,15 @@ class WelcomeView: UIViewController, UICollectionViewDelegate, UICollectionViewD
         // Do any additional setup after loading the view.
     }
     
-    var RecommandedImage = ["Adorrgon", "AICOK", "Amazon", "Dewalt", "Duracell", "Fairywill", "Fosil", "Newman's", "Samsung_Laptop", "Skechers"]
+    //var RecommandedImage = ["Adorrgon", "AICOK", "Amazon", "Dewalt", "Duracell", "Fairywill", "Fosil", "Newman's", "Samsung_Laptop", "Skechers"]
     
-    var ItemName = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"]
+    var itemList = DBHelper.inst.getItems()
     
-    var DealsImage = ["BLINKHOMESECURITY", "DXLOVER", "HOMPOW", "MADAXY", "NETGEAR", "SAMSUNG", "Solimo", "TOMETIC", "TRYVAT", "Yeedi"]
+    //var ItemName = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"]
     
-    var ItemName2 = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"]
+    //var DealsImage = ["BLINKHOMESECURITY", "DXLOVER", "HOMPOW", "MADAXY", "NETGEAR", "SAMSUNG", "Solimo", "TOMETIC", "TRYVAT", "Yeedi"]
+    
+   // var ItemName2 = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"]
     
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -39,10 +41,10 @@ class WelcomeView: UIViewController, UICollectionViewDelegate, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == self.Recommandations{
-            return RecommandedImage.count
+            return itemList.count
         }
         else{
-            return DealsImage.count
+            return itemList.count
         }
         
         
@@ -52,13 +54,13 @@ class WelcomeView: UIViewController, UICollectionViewDelegate, UICollectionViewD
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         if collectionView == self.Recommandations{
-            DBHelper.inst.holdCurrentItem(name: ItemName[indexPath.item])
+            DBHelper.inst.holdCurrentItem(name: itemList[indexPath.item].name!)
             let Bienvenue = storyboard?.instantiateViewController(withIdentifier: "ItemBoard") as! ItemCtrl
             present(Bienvenue, animated: true, completion: nil)
             
         }
         else {
-            DBHelper.inst.holdCurrentItem(name: ItemName2[indexPath.item])
+            DBHelper.inst.holdCurrentItem(name: itemList[indexPath.item].name!)
             let Bienvenue = storyboard?.instantiateViewController(withIdentifier: "ItemBoard") as! ItemCtrl
             present(Bienvenue, animated: true, completion: nil)
             
@@ -68,17 +70,19 @@ class WelcomeView: UIViewController, UICollectionViewDelegate, UICollectionViewD
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if collectionView == self.Recommandations{
-            
+            print("Doing Recommendations")
             let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! WelcomeViewCell
-            cell1.ReImage.image = UIImage(named: RecommandedImage[indexPath.item])
-            cell1.ReText.text = ItemName[indexPath.item]
+            cell1.ReImage.image = UIImage(named: itemList[indexPath.item].image!)
+            cell1.ReText.text = itemList[indexPath.item].name!
             return cell1
             
         }
         else {
+            print("Doing Deals")
+            print(itemList[indexPath.item].image!)
             let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! WelcomeViewCell
-            cell2.DealsImg.image = UIImage(named: DealsImage[indexPath.item])
-            cell2.DealsText.text = ItemName2[indexPath.item]
+            cell2.DealsImg.image = UIImage(named: itemList[indexPath.item].image!)
+            cell2.DealsText.text = itemList[indexPath.item].name!
             return cell2
         }
         
