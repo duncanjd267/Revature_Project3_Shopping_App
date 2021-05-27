e12730c4697c9cdd82f5ddfcf3d2f7b6c5f4f534
duncanjd267 <duncanjd267@gmail.com>
2021-05-27 10:37:30 -0400

    All images and Items can be added

--- Project_3/Project_3/LaunchingViewController.swift
+++ Project_3/Project_3/LaunchingViewController.swift
@@ -43,6 +43,54 @@ class LaunchingViewController: UIViewController {
         DBHelper.inst.addItem(object: dic, price: 799.99)
         
         //Gucci
+        dic = ["name": "Black Gucci Bag", "description": "Black Gucci bag is the perfect walking bag", "category": "Gucci", "image": "BGucci1", "image2" : "BGucci2", "image3" : "BGucci3"]
+        DBHelper.inst.addItem(object: dic, price: 1999.99)
+        dic = ["name": "Dark Blue Gucci handbag", "description": "Small and affordable dark blue handbag", "category": "Gucci", "image": "DBGucci1", "image2" : "DBGucci1", "image3" : "DBGucci1"]
+        DBHelper.inst.addItem(object: dic, price: 3999.99)
+        dic = ["name": "Blue Gucci Bag", "description": "Blue Gucci bag is the perfect walking bag with a stylish design", "category": "Gucci", "image": "BGucci1", "image2" : "BGucci2", "image3" : "BGucci3"]
+        DBHelper.inst.addItem(object: dic, price: 999.99)
+        dic = ["name": "Polka Dot Gucci Bag", "description": "Polka Dot bag is the perfect walking bag with a stylish design", "category": "Gucci", "image": "PDGucci1", "image2" : "PDGucci2", "image3" : "PDGucci3"]
+        DBHelper.inst.addItem(object: dic, price: 399.99)
+        dic = ["name": "Red Gucci Bag", "description": "Red Gucci bag is the perfect walking bag", "category": "Gucci", "image": "RGucci1", "image2" : "RGucci2", "image3" : "RGucci3"]
+        DBHelper.inst.addItem(object: dic, price: 2999.99)
+        //Nike
+        
+        dic = ["name": "Black Grey Nike shoes", "description": "Black Grey shoes perfect for running and outdoor activities", "category": "Nike", "image": "BGrey_Nike1", "image2" : "BGrey_Nike2", "image3" : "BGrey_Nike3"]
+        DBHelper.inst.addItem(object: dic, price: 19.99)
+        dic = ["name": "Pink Nike shoes", "description": "Pink shoes perfect for running and outdoor activities, lifting as well", "category": "Nike", "image": "Pink_Nike1", "image2" : "Pink_Nike2", "image3" : "Pink_Nike3"]
+        DBHelper.inst.addItem(object: dic, price: 39.99)
+        dic = ["name": "Black Nike shoes", "description": "Black shoes perfect for running and outdoor activities", "category": "Nike", "image": "Black_Nike1", "image2" : "Black_Nike2", "image3" : "Black_Nike3"]
+        DBHelper.inst.addItem(object: dic, price: 99.99)
+        dic = ["name": "Red Nike shoes", "description": "Red shoes perfect for running and outdoor activities, lifting as well", "category": "Nike", "image": "RBlack_Nike1", "image2" : "RBlack_Nike2", "image3" : "RBlack_Nike3"]
+        DBHelper.inst.addItem(object: dic, price: 79.99)
+        dic = ["name": "White Addidas shoes", "description": "White shoes perfect for running and outdoor activities, lifting as well", "category": "Nike", "image": "White_Nike1", "image2" : "White_Nike2", "image3" : "White_Nike3"]
+        DBHelper.inst.addItem(object: dic, price: 29.99)
+        
+        //HP
+        
+        dic = ["name": "Black HP Computer", "description": "Black HP Computer perfect for work or play", "category": "HP", "image": "BComputer1", "image2" : "BComputer2", "image3" : "BComputer3"]
+        DBHelper.inst.addItem(object: dic, price: 799.99)
+        dic = ["name": "Black Grey HP Computer", "description": "Black Grey HP Computer perfect for work or play", "category": "HP", "image": "BGComputer1", "image2" : "BGComputer2", "image3" : "BGComputer3"]
+        DBHelper.inst.addItem(object: dic, price: 699.99)
+        dic = ["name": "Blue HP Computer", "description": "Blue HP Computer perfect for the casual user", "category": "HP", "image": "BlComputer1", "image2" : "BlComputer2", "image3" : "BlComputer3"]
+        DBHelper.inst.addItem(object: dic, price: 499.99)
+        dic = ["name": "Pink HP Computer", "description": "Pink HP Computer perfect for everyday use", "category": "HP", "image": "PComputer1", "image2" : "PComputer2", "image3" : "PComputer3"]
+        DBHelper.inst.addItem(object: dic, price: 399.99)
+        dic = ["name": "Red HP Computer", "description": "Red HP Computer is expensive, for high performance", "category": "HP", "image": "RComputer1", "image2" : "RComputer2", "image3" : "RComputer3"]
+        DBHelper.inst.addItem(object: dic, price: 2999.99)
+        
+        //PS3
+        dic = ["name": "Birds of Steel", "description": "Game with planes", "category": "PS3", "image": "BPS31", "image2" : "BPS32", "image3" : "BPS33"]
+        DBHelper.inst.addItem(object: dic, price: 9.99)
+        dic = ["name": "MLB the Show", "description": "Baseball Game", "category": "PS3", "image": "MLBPS31", "image2" : "MLBPS32", "image3" : "MLBPS33"]
+        DBHelper.inst.addItem(object: dic, price: 29.99)
+        dic = ["name": "Minecraft PS3 edition", "description": "Best selling videogame of all time", "category": "PS3", "image": "MPS31", "image2" : "MPS32", "image3" : "MPS33"]
+        DBHelper.inst.addItem(object: dic, price: 49.99)
+        dic = ["name": "Persona 5", "description": "Anime people fighting at school", "category": "PS3", "image": "PPS31", "image2" : "PPS32", "image3" : "PPS33"]
+        DBHelper.inst.addItem(object: dic, price: 39.99)
+        dic = ["name": "Sonic Collection", "description": "Sonic collection which has over 15 games from the franchise", "category": "PS3", "image": "SPS31", "image2" : "SPS32", "image3" : "SPS33"]
+        DBHelper.inst.addItem(object: dic, price: 5.99)
+        
     }
     
     /*
