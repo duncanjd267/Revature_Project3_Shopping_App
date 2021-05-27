9952d6deeb4a9e6c5f4c09c665c676e859969c77
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-27 11:15:40 -0400

    Add User functionality

--- Project_3/Project_3/SignupCtrl.swift
+++ Project_3/Project_3/SignupCtrl.swift
@@ -11,6 +11,9 @@ class SignupCtrl: UIViewController
 {
 	@IBOutlet weak var UsrField: UITextField!
 	@IBOutlet weak var PassField: UITextField!
+	@IBOutlet weak var TeleField: UITextField!
+	
+	let popBack = { }
 	
     override func viewDidLoad()
 	{
@@ -20,6 +23,43 @@ class SignupCtrl: UIViewController
     
 	@IBAction func Confirm(_ sender: UIButton)
 	{
+		let existing = DBHelper.inst.getUsers()
+		let usrN = UsrField.text!
+		
+		if usrN.isEmpty
+		{ // If they didn't enter a username why keep going?
+			print("Username empty.")
+			return
+		}
+		
+		for person in existing
+		{ // Checking if the name is taken
+			if person.username! == usrN
+			{
+				print("Username taken. try again.")
+				return
+			}
+		}
+		
+		let pasW = PassField.text!
+		
+		if pasW.isEmpty
+		{ // Can't have a nil password
+			print("Password empty.")
+			return
+		}
+		
+		var phone = TeleField.text!
+		
+		if phone.isEmpty
+		{ // Need a phone number
+			phone = " "
+		}
+		
+		let UsrInfo = ["username": usrN, "password": pasW, "phone": phone]
+		
+		print("Calling addUser.")
+		DBHelper.inst.addUser(object: UsrInfo)
 		
 	}
 }
