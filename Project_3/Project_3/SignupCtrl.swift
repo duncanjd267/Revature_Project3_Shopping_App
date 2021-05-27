a774802e6f956e8a05d529e5ed8b4a87f175acd4
KingWilliam-I <jrdmarriner8@gmail.com>
2021-05-27 10:48:34 -0400

    added SignupCtrl, hooked up to new user screen

---
+++ Project_3/Project_3/SignupCtrl.swift
@@ -0,0 +1,25 @@
+//
+//  SignupCtrl.swift
+//  Project_3
+//
+//  Created by admin on 5/27/21.
+//
+
+import UIKit
+
+class SignupCtrl: UIViewController
+{
+	@IBOutlet weak var UsrField: UITextField!
+	@IBOutlet weak var PassField: UITextField!
+	
+    override func viewDidLoad()
+	{
+        super.viewDidLoad()
+
+    }
+    
+	@IBAction func Confirm(_ sender: UIButton)
+	{
+		
+	}
+}
