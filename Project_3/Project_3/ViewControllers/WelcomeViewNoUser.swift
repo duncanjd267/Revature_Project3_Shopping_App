505a81d199fe544d7665a5252f664a9564d6dae8
kangseok0102 <58051320+kangseok0102@users.noreply.github.com>
2021-06-01 14:59:21 -0400

    checking conflicts

---
+++ Project_3/Project_3/ViewControllers/WelcomeViewNoUser.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewNoUser.swift
+//  Project_3
+//
+//  Created by Kangseok Lee on 6/1/21.
+//
+
+import UIKit
+
+class WelcomeViewNoUser: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+
+}
