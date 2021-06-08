ae4463d083dc170b33aaca814054bb6309c3f4f5
KingWilliam-I <jrdmarriner8@gmail.com>
2021-06-08 10:47:41 -0400

    Began work on Password recovery

--- Project_3/Project_3/ViewControllers/ForgotPwCtrl.swift
+++ Project_3/Project_3/ViewControllers/ForgotPwCtrl.swift
@@ -7,20 +7,39 @@
 
 import UIKit
 
-class ForgotPwCtrl: UIViewController {
-
-    override func viewDidLoad() {
+class ForgotPwCtrl: UIViewController
+{
+	@IBOutlet weak var EmailField: UITextField!
+	
+    override func viewDidLoad()
+	{
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func SignIn(_ sender: Any) {
+    @IBAction func SignIn(_ sender: Any)
+	{
         
         let vc = storyboard?.instantiateViewController(withIdentifier: "LogInPage") as! LoginCtrl
         present(vc, animated: true, completion: nil)
         
     }
     
-
+	@IBAction func Recover(_ sender: UIButton)
+	{
+		var address: String?
+		
+		if let thing = EmailField!.text, thing.isEmpty
+		{
+			return // Can't do anything with an empty field
+		}
+		else
+		{
+			address = EmailField!.text
+		}
+		
+		
+	}
+	
 }
